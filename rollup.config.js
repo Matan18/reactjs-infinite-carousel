bc251a4f7aa1c03543a20d682561ff3dda5bb3c2
Mateus Andriola <mateusandriola18@gmail.com>
2021-05-15 15:33:18 -0300

    Removing react-scripts

--- rollup.config.js
+++ rollup.config.js
@@ -6,30 +6,28 @@ import path from "path";
 const PACKAGE_ROOT_PATH = process.cwd();
 const INPUT_FILE = path.join(PACKAGE_ROOT_PATH, 'src/index.ts');
 
-export default [
-  {
-    input: INPUT_FILE,
-    output: [
-      {
-        file: 'playground/src/component-lib/index.js',
-        format: 'esm',
-        banner: '/* eslint-disable */',
-      },
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'esm' },
-    ],
-    plugins: [
-      del({ targets: ['dist/*', 'playground/src/component-lib'] }),
-      typescript({
-        rollupCommonJSResolveHack: false,
-        clean: true,
-        tsconfigOverride: {
-          compilerOptions: {
-            declarationDir: path.resolve(PACKAGE_ROOT_PATH, './typings'),
-          },
+export default {
+  input: INPUT_FILE,
+  output: [
+    {
+      file: 'playground/src/component-lib/index.js',
+      format: 'esm',
+      banner: '/* eslint-disable */',
+    },
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' },
+  ],
+  plugins: [
+    del({ targets: ['dist/*', 'playground/src/component-lib'] }),
+    typescript({
+      rollupCommonJSResolveHack: false,
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declarationDir: path.resolve(PACKAGE_ROOT_PATH, './typings'),
         },
-      }),
-    ],
-    external: Object.keys(pkg.peerDependencies || {}),
-  },
-];
+      },
+    }),
+  ],
+  external: Object.keys(pkg.peerDependencies || {}),
+};
